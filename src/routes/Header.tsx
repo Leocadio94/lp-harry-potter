36625a11bf5f54966bc354d9a836627aa3d1d4c1
Gabriel Leocadio <gabrielsilva94@gmail.com>
2024-02-12 00:31:30 -0300

    feat: estilização do header

--- src/routes/Header.tsx
+++ src/routes/Header.tsx
@@ -1,20 +1,32 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className="header">
-      <ul className="header__list">
-        <li className="header__item">
-          <Link to="/" className="header__link">
-            Home
-          </Link>
-        </li>
-        <li className="header__item">
-          <Link to="/sobre" className="header__link">
-            Sobre
-          </Link>
-        </li>
-      </ul>
+      <div className="container">
+        <ul className="header__list">
+          <li className="header__item">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `header__link${isActive ? " header__link--active" : ""}`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="header__item">
+            <NavLink
+              to="/sobre"
+              className={({ isActive }) =>
+                `header__link ${isActive ? "header__link--active" : ""}`
+              }
+            >
+              Sobre
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
